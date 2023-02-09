65f7341015a15c87fa269936bb3ed93418743b00
Cloris Li <cloris.lzy@gmail.com>
2023-02-09 07:19:17 -0800

    Implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  const keys = Object.keys(obj);
+  for (let key of keys) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+//Test code
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
