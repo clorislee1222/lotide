549380ccf5bb256d53a75d2e72d4209ca510ddab
Cloris Li <cloris.lzy@gmail.com>
2023-02-14 11:39:51 -0800

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️ Assertion Passed: ${actual}  === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const eqArrays = function(a1, a2) {
   if (a1.length !== a2.length) {
@@ -18,7 +13,4 @@ const eqArrays = function(a1, a2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
