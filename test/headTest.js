3d972c5baa03d563708a51a04fb09e1ff2d8f870
Cloris Li <cloris.lzy@gmail.com>
2023-02-14 22:19:18 -0800

    set up with Mocha and Chai, modified head

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,26 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+describe('#head', () => {
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
+
+describe('#head', () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5,6,7]), 5); 
+  });
+});
+
+describe('#head', () => {
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+});
