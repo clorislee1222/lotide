a428e530154fc33028c9b6ae888a652aa2fe76d4
Cloris Li <cloris.lzy@gmail.com>
2023-02-14 21:33:17 -0800

    refactored middle

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(a1, a2) {
-  if (a1.length !== a2.length) {
-    return false;
-  } 
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(a1, a2) {
-  if (eqArrays(a1, a2)) {
-    console.log(`✔️ ${a1} === ${a2}`);
-  } else {
-    console.log(`❌ ${a1} !== ${a2}`);
-  }
-};
-
 const middle = function(a) {
   let result = [];
   let arrayLength = a.length;
@@ -35,10 +15,4 @@ const middle = function(a) {
   }
 };
 
-// Test code
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
