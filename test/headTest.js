64e24ac90ce27fd59c7725e0687bee6ea990a3dd
Cloris Li <cloris.lzy@gmail.com>
2023-02-14 23:00:25 -0800

    modified middleTest and tailTest with Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -5,21 +5,12 @@ describe('#head', () => {
   it('returns 1 for [1, 2, 3]', () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-});
-
-describe('#head', () => {
   it("returns '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5'); 
   });
-});
-
-describe('#head', () => {
   it("returns 5 for [5, 6, 7]", () => {
     assert.strictEqual(head([5,6,7]), 5); 
   });
-});
-
-describe('#head', () => {
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
   });
