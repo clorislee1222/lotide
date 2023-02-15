8ebe60d7aece556589d9fd54b2f041797135fd11
Cloris Li <cloris.lzy@gmail.com>
2023-02-14 21:27:26 -0800

    refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️ Assertion Passed: ${actual}  === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(a1, a2) {
-  if (a1.length !== a2.length) {
-    return false;
-  } 
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(a1, a2) {
   if (eqArrays(a1, a2)) {
@@ -25,3 +7,5 @@ const assertArraysEqual = function(a1, a2) {
     console.log(`❌ ${a1} !== ${a2}`);
   }
 };
+
+module.exports = assertArraysEqual;
